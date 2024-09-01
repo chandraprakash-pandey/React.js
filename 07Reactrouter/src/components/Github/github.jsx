87cbf9ff450cbf6370375ca4fe79c6605678eadd
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2024-09-01 17:02:45 +0530

    React Router

--- 07Reactrouter/src/components/Github/github.jsx
+++ 07Reactrouter/src/components/Github/github.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/chandraprakash-pandey')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            setData(data)
-        })
-    })
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/chandraprakash-pandey')
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // })
+    
     return (
         <div className='p-4 text-3xl text-white bg-gray-500'>
         <h1>Followers: {data.followers}</h1>
@@ -21,3 +22,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/chandraprakash-pandey')
+    return response.json()
+}
--- 07Reactrouter/src/main.jsx
+++ 07Reactrouter/src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/github.jsx'
+import Github, { githubInfoLoader } from './components/Github/github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -40,7 +40,10 @@ const router = createBrowserRouter(
       <Route path='About' element={<About/>} />
       <Route path='Contact' element={<Contact/>} />
       <Route path='User/:Userid' element={<User/>} />
-      <Route path='github' element={<Github/>} />
+      <Route
+      loader={githubInfoLoader} 
+      path='github' 
+      element={<Github/>} />
     </Route>
   )
 )
